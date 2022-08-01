5426885b53e7121dbfc92d1bed2b1c4d13a159ba
qrobot <qrobot>
2022-08-01 21:56:19 +0800

    edit lie group math tool

--- lib/math_tool.h
+++ lib/math_tool.h
@@ -1,4 +1,42 @@
 #include <ATen/core/TensorBody.h>
 #include <torch/torch.h>
 
+torch::Tensor vec2SO3(torch::Tensor vec);
+/**
+ * formula: A = a^
+ *
+ * rotation vector to skew symmetric matrix
+ * vector(3) φ {r,p,y} to SO(3) Φ {{ 0, -y,  p},
+ *                                 { y,  0, -r},
+ *                                 {-p,  r,  0}}
+ * **/
+torch::Tensor vec2SE3(torch::Tensor vec);
+
+torch::Tensor SO32vec(torch::Tensor SO3);
+/**
+ * formula : a = Aˇ
+ *
+ * skew symmetric matrix to rotation vector
+ *
+ * SO(3) Φ {{ 0, -y,  p}, to rotation vector(3) φ {r,p,y}
+ *          { y,  0, -r},
+ *          {-p,  r,  0}}
+ * **/
+torch::Tensor SE32vec(torch::Tensor SE3);
+
+torch::Tensor so32SO3(torch::Tensor so3);
+/**
+ * formula : R = exp(Φ)
+ *
+ * skew symmetric matrix to
+ *
+ * rotation matrix(3X3) Φ {{ 0, -y,  p}, to rotation vector(3) φ {r,p,y}
+ *                         { y,  0, -r},
+ *                         {-p,  r,  0}}
+ * **/
+torch::Tensor se32SE3(torch::Tensor se3);
+
+torch::Tensor SO32so3(torch::Tensor SO3);
+torch::Tensor SE32se3(torch::Tensor SE3);
+
 torch::Tensor adj(torch::Tensor T);
